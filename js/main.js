9567f6a0fb5937aa475c817ef304ee911f80110a
William Dunston <wedunsto@aggies.ncat.edu>
2023-01-02 12:33:35 -0500

    Updated app

---
+++ js/main.js
@@ -0,0 +1,68 @@
+
+window.onload = function () {
+	//Allow the back button to close the application
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName == "back")
+	try {
+	    tizen.application.getCurrentApplication().exit();
+	} catch (ignore) {
+	}
+    });
+    
+    //Initialize the counter values
+    let life_points = 40;
+    let poison_counters = 0;
+    let counter = "life";
+    
+    //Place the initial life counter value on the screen
+    box = document.querySelector('#textbox');
+    box.innerHTML = life_points.toString();
+    
+    //Event handler for the rotary dial
+    document.addEventListener('rotarydetent', function(ev) {
+        var direction = ev.detail.direction;
+        
+        if (counter == "reset"){
+        	life_points = 40;
+            poison_counters = 0;
+        }
+
+        if (direction == 'CW' & coutner == "life") {
+        	life_points = life_points + 1;
+        	box.innerHTML = life_points.toString();
+        }
+        else if (direction == 'CW' & counter == "poison" & poison_counters < 10){
+        	poison_counters = poison_counters + 1;
+        	box.innerHTML = poison_counters.toString();
+        }
+        
+        if (direction == 'CCW' & counter == "life" & life_points > 0 ) {
+        	life_points = life_points - 1;
+        	box.innerHTML = life_points.toString();
+        }
+        else if(direction == 'CCW' & counter == "poison" & poison_counters > 0){
+        	poison_counters = poison_counters - 1;
+        	box.innerHTML = poison_counters.toString();
+        }
+    });
+    
+    //Event handler for screen press
+    var textbox = document.querySelector('.contents');
+    textbox.addEventListener("click", function(){
+    	if (counter == "life"){
+    		counter = "poison";
+    		box.innerHTML = poison_counters.toString();
+    	}
+    	else if (counter == "poison"){
+    		counter = "reset";
+    		box.innerHTML = "Reset";
+    	}
+    	else{
+    		counter = "life";
+    		box.innerHTML = life_points.toString();
+    	}
+    });
+    
+    tizen.power.request('SCREEN', 'SCREEN_NORMAL');
+    tizen.power.request('CPU','CPU_AWAKE');
+};
